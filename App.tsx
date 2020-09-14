5363895df31f5349856d4f7d582d408395bef1c3
vickonrails <victorofoegbu0009@gmail.com>
2020-09-14 01:04:36 +0100

    end of module 01

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { Onboarding } from "./src/authentication/onboarding";
+import { LoadAssets } from "./src/authentication/onboarding/components";
+
+const fonts = {
+  "SFProText-Bold": require("./assets/fonts/SF-Pro-Text-Bold.otf"),
+  "SFProText-Semibold": require("./assets/fonts/SF-Pro-Text-Semibold.otf"),
+  "SFProText-Regular": require("./assets/fonts/SF-Pro-Text-Regular.otf"),
+};
+
+const AuthenticationStack = createStackNavigator();
+
+const { Screen, Navigator } = AuthenticationStack;
+
+const AuthenticationNavigator = () => (
+  <Navigator headerMode="none">
+    <Screen name="Onboarding" component={Onboarding} />
+  </Navigator>
+);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoadAssets {...{ fonts }}>
+      <AuthenticationNavigator />
+    </LoadAssets>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
