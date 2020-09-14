702f3b1312088fad2e8a705fa51f07a5fbd07433
vickonrails <victorofoegbu0009@gmail.com>
2020-09-14 19:38:10 +0100

    style: add animation on horizontal scroll

--- src/authentication/onboarding/onboarding.tsx
+++ src/authentication/onboarding/onboarding.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { View, StyleSheet, Dimensions } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import Animated from "react-native-reanimated";
+import {
+  useValue,
+  onScrollEvent,
+  interpolateColor,
+} from "react-native-redash/lib/module/v1";
 
 import Slide, { SLIDE_HEIGHT } from "./slide";
 
@@ -21,25 +26,35 @@ const styles = StyleSheet.create({
 });
 
 export const Onboarding = () => {
+  const x = useValue(0);
+  // TODO: useScrollEvent?
+  const onScroll = onScrollEvent({ x });
+  const backgroundColor = interpolateColor(x, {
+    inputRange: [0, width, width * 2, width * 3],
+    outputRange: ["#BFEAF5", "#BEECC4", "#FFE4D9", "#FFDDDD"],
+  });
+
   return (
     <View style={styles.container}>
-      <View style={styles.slider}>
-        <ScrollView
+      <Animated.View style={[styles.slider, { backgroundColor }]}>
+        <Animated.ScrollView
           horizontal
           snapToInterval={width}
           decelerationRate="fast"
           showsHorizontalScrollIndicator={false}
           bounces={false}
+          scrollEventThrottle={1}
+          {...{ onScroll }}
         >
           <Slide label="Relaxed" />
           <Slide label="Playful" right />
           <Slide label="Eccentric" />
           <Slide label="Funky" right />
-        </ScrollView>
-      </View>
+        </Animated.ScrollView>
+      </Animated.View>
       <View style={styles.footer}>
-        <View
-          style={{ ...StyleSheet.absoluteFillObject, backgroundColor: "cyan" }}
+        <Animated.View
+          style={{ ...StyleSheet.absoluteFillObject, backgroundColor }}
         />
         <View
           style={{ flex: 1, backgroundColor: "white", borderTopLeftRadius: 75 }}
